f9968e1fc30a025b71df4b1c9917379a5ca2d331
lzxm160 <lzxm160@sina.com>
2019-09-04 08:21:23 +0800

    for test

--- sql/storebase.go
+++ sql/storebase.go
@@ -121,10 +121,10 @@ func (s *storeBase) Transact(txFunc func(*sql.Tx) error) (err error) {
 		}
 	}()
 	err = txFunc(tx)
-	count++
 	if count == 3 {
 		err = errors.New("for test purpose")
 	}
+	count++
 
 	return err
 }
