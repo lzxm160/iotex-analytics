f626cdf4ec634d269fce64a8a720a174da1ab910
lzxm160 <lzxm160@sina.com>
2019-09-13 10:41:24 +0800

    x

--- queryprotocol/actions/protocol_test.go
+++ queryprotocol/actions/protocol_test.go
@@ -243,9 +243,6 @@ func TestProtocol(t *testing.T) {
 	t.Run("Testing GetXrc20 by sender or receiptor address", func(t *testing.T) {
 		test, errXrc := p.GetXrc20ByAddress("io13k7t6k3excy3hkyp60zjakc5le5fvvapqcwfsg", 4, 1)
 		require.NoError(errXrc)
-		fmt.Println(len(test))
-		fmt.Println(test[0])
-		fmt.Println(test[1])
 		for k := 0; k < 2; k++ {
 			require.Equal(test[k].Hash, testSituation.output[k+2].Hash)
 			require.Equal(test[k].From, testSituation.output[k+2].From)
