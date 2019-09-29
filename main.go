d67ebfb85718ab6ed23cb4060ec2146f7136e15f
lzxm160 <lzxm160@sina.com>
2019-09-29 11:57:30 +0800

    x

--- main.go
+++ main.go
@@ -101,8 +101,7 @@ func main() {
 					func(ctx context.Context, next gqlgen.Resolver) (res interface{}, err error) {
 						rc := gqlgen.GetResolverContext(ctx)
 						fmt.Println("Entered", rc.Object, rc.Field.Name)
-						res, err = next(ctx)
-						fmt.Println("Left", rc.Object, rc.Field.Name, "=>", res, err)
+						fmt.Println(rc.Args)
 						return next(ctx)
 					}),
 				//func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
