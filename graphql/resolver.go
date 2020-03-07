31e8d571ddf704d1548ea71ff09b16b32544311a
lzxm160 <lzxm160@sina.com>
2020-03-07 18:57:05 +0800

    x

--- graphql/resolver.go
+++ graphql/resolver.go
@@ -829,7 +829,7 @@ func (r *queryResolver) xrcTokenHolderAddresses(ctx context.Context, actionRespo
 		return errors.Wrap(err, "failed to get address")
 	}
 	var offset, size uint64
-	pageArgs := parseFieldArguments(ctx, "addresses", "pagination")
+	pageArgs := parseFieldArguments(ctx, "addresses", "")
 	paginationMap, err := getPaginationArgs(pageArgs)
 	switch {
 	default:
