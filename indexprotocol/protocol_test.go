cd636e95c1475352375639569267ee1767158191
lzxm160 <lzxm160@sina.com>
2020-05-14 18:29:04 +0800

    x

--- indexprotocol/protocol_test.go
+++ indexprotocol/protocol_test.go
@@ -7,7 +7,14 @@
 package indexprotocol
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
+
+	"github.com/iotexproject/iotex-core/pkg/log"
+	"github.com/iotexproject/iotex-proto/golang/iotexapi"
+	"google.golang.org/grpc"
 
 	"github.com/stretchr/testify/require"
 )
@@ -22,4 +29,26 @@ func TestEnDecodeName(t *testing.T) {
 	encoded, err := EncodeDelegateName(decoded)
 	require.NoError(err)
 	require.Equal(candidateName, encoded)
+
+	/////////////////////////////////
+	chainEndpoint := "35.236.100.38:14014"
+	grpcCtx1, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	conn1, err := grpc.DialContext(grpcCtx1, chainEndpoint, grpc.WithBlock(), grpc.WithInsecure())
+	if err != nil {
+		log.L().Error("Failed to connect to chain's API server.")
+	}
+	chainClient := iotexapi.NewAPIServiceClient(conn1)
+	cand, err := GetAllStakingCandidates(chainClient, 0)
+	require.NoError(err)
+	for _, c := range cand.Candidates {
+		fmt.Println(c)
+	}
+
+	fmt.Println("/////////////////////////////////")
+	buckets, err := GetAllStakingBuckets(chainClient, 0)
+	require.NoError(err)
+	for _, c := range buckets.Buckets {
+		fmt.Println(c)
+	}
 }
