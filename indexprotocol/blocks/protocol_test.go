f57fff814e75a6c032b2396030ba015d86c8d890
lzxm160 <lzxm160@sina.com>
2020-05-27 13:56:41 +0800

    fix delegate no name

--- indexprotocol/blocks/protocol_test.go
+++ indexprotocol/blocks/protocol_test.go
@@ -52,7 +52,7 @@ func TestProtocol(t *testing.T) {
 		require.NoError(store.Stop(ctx))
 	}()
 
-	p := NewProtocol(store, epochctx.NewEpochCtx(1, 1, 1, epochctx.FairbankHeight(100000)), indexprotocol.GravityChain{})
+	p := NewProtocol(store, epochctx.NewEpochCtx(1, 1, 1, epochctx.FairbankHeight(100000)), indexprotocol.GravityChain{GravityChainStartHeight: 1})
 
 	require.NoError(p.CreateTables(ctx))
 
