03ce73b44a57f2b2ff0bc416f4e658cfdd5cc620
lzxm160 <lzxm160@sina.com>
2020-05-27 16:28:46 +0800

    fix comments

--- indexprotocol/blocks/protocol_test.go
+++ indexprotocol/blocks/protocol_test.go
@@ -70,9 +70,9 @@ func TestProtocol(t *testing.T) {
 	readStateRequest := &iotexapi.ReadStateRequest{
 		ProtocolID: []byte(indexprotocol.PollProtocolID),
 		MethodName: []byte("GetGravityChainStartHeight"),
-		Arguments:  [][]byte{[]byte(strconv.FormatUint(1, 10))},
+		Arguments:  [][]byte{[]byte(strconv.FormatUint(blk1.Height(), 10))},
 	}
-	chainClient.EXPECT().ReadState(gomock.Any(), readStateRequest).AnyTimes().Return(&iotexapi.ReadStateResponse{
+	chainClient.EXPECT().ReadState(gomock.Any(), readStateRequest).Times(1).Return(&iotexapi.ReadStateResponse{
 		Data: []byte(strconv.FormatUint(1000, 10)),
 	}, nil)
 	electionClient.EXPECT().GetCandidates(gomock.Any(), gomock.Any()).Times(2).Return(
