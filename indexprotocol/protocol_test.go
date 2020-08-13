a0cd71e01ccf9001d73469e55c426ee411e35e5a
lzxm160 <lzxm160@sina.com>
2020-08-13 11:17:19 +0800

    x

--- indexprotocol/protocol_test.go
+++ indexprotocol/protocol_test.go
@@ -10,8 +10,13 @@ import (
 	"context"
 	"fmt"
 	"sort"
+	"strconv"
 	"testing"
 	"time"
+
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/iotexproject/iotex-proto/golang/iotexapi"
 	"github.com/iotexproject/iotex-proto/golang/iotextypes"
@@ -66,5 +71,45 @@ func TestGetAllStakingBuckets(t *testing.T) {
 	require.NoError(err)
 	for _, r := range resp.GetBuckets() {
 		fmt.Println(r)
+	}
+}
+
+func TestProbation(t *testing.T) {
+	require := require.New(t)
+	grpcCtx1, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	conn1, err := grpc.DialContext(grpcCtx1, "api.iotex.one:80", grpc.WithBlock(), grpc.WithInsecure())
+	require.NoError(err)
+	chainClient := iotexapi.NewAPIServiceClient(conn1)
+	for i := uint64(22); i < 52; i++ {
+		//time.Sleep(time.Second * 5)
+		request := &iotexapi.ReadStateRequest{
+			ProtocolID: []byte("poll"),
+			MethodName: []byte("ProbationListByEpoch"),
+			//Arguments:  [][]byte{[]byte(strconv.FormatUint(11358+i, 10))},
+			Height: strconv.FormatUint(6360841+i*719, 10),
+		}
+		out, err := chainClient.ReadState(context.Background(), request)
+		if err != nil {
+			sta, ok := status.FromError(err)
+			if ok && sta.Code() == codes.NotFound {
+				fmt.Println("codes.NotFound")
+				return
+			}
+			fmt.Println(err)
+			return
+		}
+		probationList := &iotextypes.ProbationCandidateList{}
+		if out.Data != nil {
+			if err := proto.Unmarshal(out.Data, probationList); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+		fmt.Println(i, len(probationList.ProbationList))
+		for _, p := range probationList.ProbationList {
+			fmt.Println(p)
+			require.Equal(p.Address, "io135tn3x8tee9jyxsrmtpgfnectc3u85hthak4u9")
+		}
 	}
 }
